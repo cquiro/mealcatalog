6a4972d510ef44ea4eb37848b8888e07153be738
Carlos <cquiro@gmail.com>
2025-08-06 22:02:47 +0200

    Fix typo

--- src/main/kotlin/com/cquiro/mealcatalog/domain/entities/Unit.kt
+++ src/main/kotlin/com/cquiro/mealcatalog/domain/entities/Unit.kt
@@ -3,7 +3,7 @@ package com.cquiro.mealcatalog.domain.entities
 enum class Unit(val fullName: String, val symbol: String) {
     GRAM("Gram", "g"),
     TEASPOON("Teaspoon", "tsp"),
-    TABLESPOON("tablespoon", "tbsp"),
+    TABLESPOON("Tablespoon", "tbsp"),
     CUP("Cup", "cup"),
     MILLILITER("Milliliter", "mL"),
     LITER("Liter", "L"),
